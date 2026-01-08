f9d734b3cee37da9351427b94a8f9304386d0d65
BlimBlamBloom <bassoonami@gmail.com>
2026-01-08 07:04:50 -0800

    Create lesson2-data.js

---
+++ spanish/lessons/lesson2-data.js
@@ -0,0 +1,106 @@
+export const lessonData = {
+    lessonNumber: 2,
+    title: "Lección 2",
+    videoId: "OLAHJKGPtC4",
+    
+    conversation: {
+        spanish: `Alejandro: Buenos días.
+Polly: Buenos días.
+Alejandro: ¿Adónde vas?
+Polly: Voy a la biblioteca. ¿Adónde vas?
+Alejandro: Voy a mi universidad. ¿Te gustan los libros?
+Polly: Sí, me gustan los libros. ¿Te gusta la escuela?
+Alejandro: No, no me gusta la escuela.`,
+        
+        chinese: `Alejandro：早安。
+Polly：早安。
+Alejandro：你要去哪裡？
+Polly：我要去圖書館。你要去哪裡？
+Alejandro：我要去我的大學。你喜歡書嗎？
+Polly：是的，我喜歡書。你喜歡上學嗎？
+Alejandro：不，我不喜歡上學。`
+    },
+    
+    vocabulary: `Buenos días - 早安
+ ¿Adónde? - 要去哪裡
+ ir (voy / vas) - 去（我去 / 你去）
+ biblioteca - 圖書館
+ universidad - 大學
+ gustar (me gusta / te gusta) - 喜歡（我喜歡 / 你喜歡）
+ libros - 書 
+ escuela - 學校
+ sí - 是的
+ no - 不`,
+    
+    questionsAndAnswers: `¿Adónde vas? 你要去哪裡？
+ Voy a ______. 我要去 ______。
+
+<u>Vocabulario 2</u>
+cine – 電影院
+restaurante – 餐廳
+tienda – 商店
+banco – 銀行
+estación de tren – 火車站
+aeropuerto – 機場
+playa – 海灘
+montaña – 山
+museo – 博物館
+hotel – 飯店
+        
+¿Te gusta ____? 你喜歡___嗎？(單數)
+ Sí, me gusta ___. / No, no me gusta ___. 是的，我喜歡__。/ 不，我不喜歡__。
+¿Te gustan ____? 你喜歡___嗎？(複數)
+Sí, me gustan ___. / No, no me gustan ___. 是的，我喜歡__。/ 不，我不喜歡 
+
+<u>Vocabulario 3</u>
+música – 音樂
+películas – 電影
+té – 茶
+fútbol – 足球
+perros – 狗
+gatos – 貓
+bicicleta – 自行車
+chocolate – 巧克力
+fruta – 水果
+helado – 冰淇淋`,
+    
+    quizVocab: {
+        "música": ["音樂"],
+        "películas": ["電影"],
+        "té": ["茶"],
+        "fútbol": ["足球"],
+        "perros": ["狗"],
+        "gatos": ["貓"],
+        "bicicleta": ["自行車"],
+        "chocolate": ["巧克力"],
+        "fruta": ["水果"],
+        "helado": ["冰淇淋"],
+        "cine": ["電影院"],
+        "restaurante": ["餐廳"],
+        "tienda": ["商店"],
+        "banco": ["銀行"],
+        "estación de tren": ["火車站"],
+        "aeropuerto": ["機場"],
+        "playa": ["海灘"],
+        "montaña": ["山"],
+        "museo": ["博物館"],
+        "hotel": ["飯店"],
+        "Buenos días": ["早安"],
+        "¿Adónde?": ["要去哪裡"],
+        "voy": ["我去"],
+        "vas": ["你去"],
+        "biblioteca": ["圖書館"],
+        "universidad": ["大學"],
+        "me gusta": ["我喜歡"],
+        "te gusta": ["你喜歡"],
+        "libros": ["書"],
+        "escuela": ["學校"],
+        "sí": ["是的"],
+        "no": ["不"]
+    },
+    
+    navigation: {
+        prev: { text: "← Lección 1", url: "lesson1.html" },
+        next: { text: "Lección 3 →", url: "lesson3.html" }
+    }
+};
