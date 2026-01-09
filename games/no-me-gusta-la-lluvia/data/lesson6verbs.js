55650dbd853102fe4b8f873a66af1ba39735d89d
BlimBlamBloom <bassoonami@gmail.com>
2026-01-08 16:30:08 -0800

    Create lesson6verbs.js

---
+++ games/no-me-gusta-la-lluvia/data/lesson6verbs.js
@@ -0,0 +1,56 @@
+// js/data/verbs.js
+
+// Export means "make this available to other files"
+export const verbs = {
+    hablar: { 
+        yo: 'hablo', 
+        tú: 'hablas', 
+        ella: 'habla', 
+        nosotros: 'hablamos', 
+        vosotros: 'habláis', 
+        ellos: 'hablan' 
+    },
+    escuchar: { 
+        yo: 'escucho', 
+        tú: 'escuchas', 
+        ella: 'escucha', 
+        nosotros: 'escuchamos', 
+        vosotros: 'escucháis', 
+        ellos: 'escuchan' 
+    },
+    estudiar: { 
+        yo: 'estudio', 
+        tú: 'estudias', 
+        ella: 'estudia', 
+        nosotros: 'estudiamos', 
+        vosotros: 'estudiáis', 
+        ellos: 'estudian' 
+    },
+    bailar: { 
+        yo: 'bailo', 
+        tú: 'bailas', 
+        ella: 'baila', 
+        nosotros: 'bailamos', 
+        vosotros: 'bailáis', 
+        ellos: 'bailan' 
+    },
+    viajar: { 
+        yo: 'viajo', 
+        tú: 'viajas', 
+        ella: 'viaja', 
+        nosotros: 'viajamos', 
+        vosotros: 'viajáis', 
+        ellos: 'viajan' 
+    },
+    trabajar: { 
+        yo: 'trabajo', 
+        tú: 'trabajas', 
+        ella: 'trabaja', 
+        nosotros: 'trabajamos', 
+        vosotros: 'trabajáis', 
+        ellos: 'trabajan' 
+    }
+};
+
+// List of all pronouns
+export const pronouns = ['yo', 'tú', 'ella', 'nosotros', 'vosotros', 'ellos'];
